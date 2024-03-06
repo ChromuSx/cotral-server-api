36174fb0566f21a1a984a93cadf9b3d3106018f5
Giovanni Guarino <giovanni.guarino1999@gmail.com>
2024-03-06 23:13:09 +0100

    delete dist

--- dist/routes/polesRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.registerPolesRoutes = void 0;
-const polesController_1 = require("../controllers/polesController");
-const registerPolesRoutes = (fastify) => {
-    const polesController = new polesController_1.PolesController(fastify);
-};
-exports.registerPolesRoutes = registerPolesRoutes;
--- dist/routes/transitsRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.registerTransitsRoutes = void 0;
-const transitsController_1 = require("../controllers/transitsController");
-const registerTransitsRoutes = (fastify) => {
-    const transitsController = new transitsController_1.TransitsController(fastify);
-};
-exports.registerTransitsRoutes = registerTransitsRoutes;
